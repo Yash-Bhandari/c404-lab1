0bd53d191bac2ae2f7d58c4451717893a093b828
Yash Bhandari <yashaswi@cs.ualberta.ca>
2022-09-14 17:23:59 -0600

    print self

--- lab1.py
+++ lab1.py
@@ -1,5 +1,10 @@
 import requests
 
-print(requests.__version__)
+# print(requests.__version__)
 
 requests.get("http://google.com/")
+
+source = requests.get(
+    "https://raw.githubusercontent.com/Yash-Bhandari/c404-lab1/main/lab1.py"
+)
+print(source.text)
